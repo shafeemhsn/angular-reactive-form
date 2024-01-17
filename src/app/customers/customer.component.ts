d82d7f794cdce0abf4be21d6b40e68d17e094d23
Shafee Muhsin <ShafeeM@fortude.co>
2024-01-17 05:38:26 +0530

    populate-data

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
+  customerForm!: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.patchValue({ //or setValue
+      firstName: 'jack',
+      lastName: 'Harness',
+      sendCatalog: false,
+    });
+  }
+
+  save(): void {
+    // console.log(customerForm.form);
+    // console.log('Saved: ' + JSON.stringify(customerForm.value));
   }
 }
